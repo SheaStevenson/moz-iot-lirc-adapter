1a8aeb41e9917816010dffff038849b939820fc6
Unknown <therealwazzar@gmail.com>
2019-02-22 20:04:52 +1000

    Addon files. Do they work? Nobody knows!

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+https://gist.github.com/prasanthj/c15a5298eb682bde34961c322c95378b
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const {Adapter, Device} = require('gateway-addon');
+const lirc = require('lirc-client')({
+  path: '/var/run/lirc/lircd'
+});
+
+class LIRCAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, manifest.name, manifest.name);
+    addonManager.addAdapter(this);
+
+    lirc.on('connect', () => {
+      lirc.send('VERSION').then(res => {
+        console.log('LIRC Version', res);
+      });
+
+      // Get available remote configs
+      lirc.list().then(function(response) {
+        console.log("Remotes", response);
+      }).catch(function(error) {
+        console.log("Problems", error);
+      });
+
+
+    });
+
+    //for (const remote of manifest.moziot.config.devices) {
+      //this.handleDeviceAdded(new LIRCDevice(this, remote));
+    //}
+  }
+}
+
+class LIRCDevice extends Device {
+  constructor(adapter, remote) {
+    super(adapter, `lirc-${remote}`);
+
+    this.remote = remote;
+    this.name = `Remote (${remote})`;
+    this.description = `Remote (${remote})`;
+    this['@context'] = 'https://iot.mozilla.org/schemas';
+    this['@type'] = [];
+    this.addAction('wake', {label: 'Wake'});
+  }
+
+  performAction(action) {
+    if (action.name !== 'wake') {
+      //return Promise.reject('Unknown action');
+      console.log(action.name+' is not the wake command.');
+    }
+
+    return new Promise((resolve, reject) => {
+
+      lirc.sendOnce('Bauhn_Soundbar', 'KEY_POWER').catch(error => {
+        console.log(action.name);
+        if (error) {
+          reject('Command failed: ' + error);
+        } else {
+          resolve();
+        }
+      });
+
+      /*
+      lirc.wake(this.remote, (err, res) => {
+        if (err || !res) {
+          reject('Wake failed');
+          return;
+        }
+
+        resolve();
+      });
+      */
+    });
+  }
+}
+
+module.exports = (addonManager, manifest) => {
+  new LIRCAdapter(addonManager, manifest);
+};
