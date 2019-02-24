183de278ea0b10226cb92c9f1da6ac336fac6993
Unknown <therealwazzar@gmail.com>
2019-02-24 22:32:24 +1000

    Commands now add automatically

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# This doesn't work right now
+# A LIRC Adapter for the Mozilla Things Gateway
 
-Sorry about that.
+This kind of works.
+
+### Build & Testing Process
+
+1. On the Raspberry Pi (Browsed to ~):
+* `rm -rf ~/.mozilla-iot/log`
+* `rm -rf ~/.mozilla-iot/addons/lirc-adapter/`
+2. Switch to Dev machine (Browsed to project folder):
+* `./package.sh`
+* `scp -r . pi@DEVICEIPADDRESSHERE:~/.mozilla-iot/addons/lirc-adapter`
+3. Switch back to Pi
+* `sudo systemctl restart mozilla-iot-gateway.service`
+4. Wait a reasonable amount of time for the gateway to restart
+5. View logs if necessary
+* `(cd ~/.mozilla-iot/log && cat "$(ls -1rt | tail -n1)")`
 
 ### Useful Links
 
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const lirc = require('lirc-client')({
   path: '/var/run/lirc/lircd'
 });
 
+const debug_mode = true;
+
 class LIRCAdapter extends Adapter {
   constructor(addonManager, manifest) {
     super(addonManager, manifest.name, manifest.name);
@@ -12,65 +14,81 @@ class LIRCAdapter extends Adapter {
     addonManager.addAdapter(adapter);
 
     lirc.on('connect', () => {
-      lirc.send('VERSION').then(res => {
-        console.log('LIRC Version Connected', res);
-      });
+      if (debug_mode) {
+        lirc.send('VERSION').then(res => {
+          console.log('LIRC Version', res);
+        });
+      }
 
       // Get available remote configs
       lirc.list().then(function(response) {
-        console.log("Remotes", typeof response, response);
+        if (debug_mode) console.log("Found Remotes", response);
 
         // Add each available remote
         for (const remote of response) {
-          console.log("Adding Remote", remote);
-          adapter.handleDeviceAdded(new LIRCDevice(adapter, remote));
+          if (debug_mode) console.log("Adding Remote", remote);
+
+          // Start getting available commands
+          lirc.list(remote).then(function(response){
+            // Set up device, and add availble commands
+            if (debug_mode) console.log("Remote has commands", remote, response);
+            adapter.handleDeviceAdded(new LIRCDevice(adapter, remote, response));
+          }).catch(function(error) {
+            if (debug_mode) console.log("Problem getting or adding commands", error);
+          });
         }
+
       }).catch(function(error) {
-        console.log("Problems", error);
+        if (debug_mode) console.log("Problem getting or adding remotes", error);
       });
     });
   }
 }
 
 class LIRCDevice extends Device {
-  constructor(adapter, remote) {
+  constructor(adapter, remote, commands) {
     super(adapter, `lirc-${remote}`);
+
+    const device = this;
 
     this.remote = remote;
     this.name = `Remote (${remote})`;
     this.description = `Remote (${remote})`;
     this['@context'] = 'https://iot.mozilla.org/schemas';
     this['@type'] = [];
-    this.addAction('key_power', {label: 'Power'});
+
+    // Add each available command
+    for (const command of commands) {
+      // Separate the code and label
+      let command_code = command.split(" ")[1];
+
+      // Process the label name
+      let command_label = command_code.replace('KEY_','').replace('_',' ').toLowerCase();
+      command_label = command_label.charAt(0).toUpperCase() + command_label.slice(1);
+
+      // Add the command
+      if (debug_mode) console.log("Adding Command", command_code, command_label);
+      this.addAction(command_code, {label: command_label});
+    }
   }
 
+
   performAction(action) {
-    if (action.name !== 'key_power') {
+    /*
+    if (action.name !== 'KEY_POWER') {
       //return Promise.reject('Unknown action');
       console.log(action.name+' is not the wake command.');
     }
+    */
 
     return new Promise((resolve, reject) => {
-
       lirc.sendOnce(this.remote, action.name).catch(error => {
-        console.log(action.name);
         if (error) {
           reject('Command failed: ' + error);
         } else {
           resolve();
         }
       });
-
-      /*
-      lirc.wake(this.remote, (err, res) => {
-        if (err || !res) {
-          reject('Wake failed');
-          return;
-        }
-
-        resolve();
-      });
-      */
     });
   }
 }
